1be45cce170a6b7c5810c0c7bc1c9051bb22b656
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-06-21 17:21:40 +0700

    layouting

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.forms.advanced-elements');
 });
 
 Route::get('/dashboard', function () {
