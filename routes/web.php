ab0f6fd519c6a9e3772931ef8a10f456939740f6
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-06-27 13:21:40 +0700

    fixing

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('admin')->group(__DIR__ . './adminRoutes.php');
+Route::prefix('admin')->group(__DIR__ . '/adminRoutes.php');
 Route::get('/', [IndexController::class, 'home'])->name('home');
 
 require __DIR__ . '/auth.php';
