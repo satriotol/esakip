3071bc45f7be74f72bda3052627640eaf9d8bf99
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-06-28 13:08:53 +0700

    fixing

--- resources/views/frontend/perencanaan_kinerja_kota/rpjmd.blade.php
+++ resources/views/frontend/perencanaan_kinerja_kota/rpjmd.blade.php
@@ -1,10 +1,12 @@
 <div class="country-lists" style="margin-top: 10px">
     <div class="row">
         <div class="col-md-12">
-            <object data="{{ asset('uploads/' . $rpjmd->file) }}" class="w-100 mt-5" style="height: 550px;width: 100%"
-                type="application/pdf">
-                <div>No online PDF viewer installed</div>
-            </object>
+            @if ($rpjmd != null)
+                <object data="{{ asset('uploads/' . $rpjmd->file) }}" class="w-100 mt-5"
+                    style="height: 550px;width: 100%" type="application/pdf">
+                    <div>No online PDF viewer installed</div>
+                </object>
+            @endif
         </div>
     </div>
 </div>
