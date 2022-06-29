db8158889ecc6fe4a2d0cf333c0095eadb333b86
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-06-29 13:13:23 +0700

    fixing

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -1,22 +1,9 @@
 <div class="footer-bottom">
     <div class="container">
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-12 text-center">
                 <p>&copy; All Rights Reserved by <a href="https://www.semarangkota.go.id/" target="_blank">Pemkot
                         Semarang</a></p>
-            </div>
-            <div class="col-md-6 text-right link">
-                <ul>
-                    <li>
-                        <a href="#">Faq</a>
-                    </li>
-                    <li>
-                        <a href="#">Contact</a>
-                    </li>
-                    <li>
-                        <a href="#">Donate</a>
-                    </li>
-                </ul>
             </div>
         </div>
     </div>
