87456f5e4cf7ccc32c8bd03e0d0299f5668faa6f
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-07-07 13:28:05 +0700

    fixing

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(1910);
+        Schema::defaultStringLength(191);
         OpdPerjanjianKinerja::observe(OpdPerjanjianKinerjaObserver::class);
     }
 }
