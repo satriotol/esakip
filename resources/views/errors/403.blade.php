2aed7142d6147f20b5e0608a01a3ccb03897b818
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-07-08 10:27:20 +0700

    error response

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -5,7 +5,7 @@
 
         <div class="row w-100 mx-0 auth-page">
             <div class="col-md-8 col-xl-6 mx-auto d-flex flex-column align-items-center">
-                <img src="{{ url('assets/images/others/404.svg') }}" class="img-fluid mb-2" alt="404">
+                <img src="{{ asset('assets/images/others/404.svg') }}" class="img-fluid mb-2" alt="404">
                 <h1 class="fw-bolder mb-22 mt-2 tx-80 text-muted">403</h1>
                 <h4 class="mb-2">Permission ??</h4>
                 <h6 class="text-muted mb-3 text-center">Oopps!! You Doesnt Have Permission</h6>
