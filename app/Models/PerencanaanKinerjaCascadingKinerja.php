24d1c4cdbf56d4cfd5903035c8f1e1d9dde2985e
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-07-13 12:01:15 +0700

    fixing

--- app/Models/PerencanaanKinerjaCascadingKinerja.php
+++ app/Models/PerencanaanKinerjaCascadingKinerja.php
@@ -11,9 +11,15 @@ class PerencanaanKinerjaCascadingKinerja extends Model
     use HasFactory;
 
     protected $fillable = ['year', 'name', 'file'];
+    protected $appends = ['file_url'];
 
     public function deleteFile()
     {
         Storage::disk('public_uploads')->delete($this->attributes['file']);
+    }
+    public function getFileUrlAttribute()
+    {
+        $file = env('ASSET_URL') .'/uploads/' . $this->file;
+        return $file;
     }
 }
