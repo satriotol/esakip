4fbaa0f5f4f6328bdc27f4ca57951a12c02b5bf4
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-07-21 20:19:29 +0700

    add download

--- resources/views/frontend/perencanaan_kinerja_kota/rpjmd.blade.php
+++ resources/views/frontend/perencanaan_kinerja_kota/rpjmd.blade.php
@@ -2,9 +2,11 @@
     <div class="row">
         <div class="col-md-12">
             @if ($rpjmd != null)
-                <object data="{{ asset('uploads/' . $rpjmd->file) }}" class="w-100 mt-5"
-                    style="height: 550px;width: 100%" type="application/pdf">
+                <object data="{{ asset('uploads/' . $rpjmd->file) }}" class="w-100 mt-5" style="height: 550px;width: 100%"
+                    type="application/pdf">
                     <div>No online PDF viewer installed</div>
+                    <p><a href="{{ asset('uploads/' . $rpjmd->file) }}" download="" target="_blank">Download File</a>
+                    </p>
                 </object>
             @endif
         </div>
