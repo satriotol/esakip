d7afd7d13ccec07c0328e975fe1a8bf42923af49
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-07-21 09:07:43 +0700

    fixing perjanjian kinerja

--- app/Http/Controllers/Api/OpdPerjanjianKinerjaController.php
+++ app/Http/Controllers/Api/OpdPerjanjianKinerjaController.php
@@ -11,15 +11,30 @@ class OpdPerjanjianKinerjaController extends Controller
 {
     public function index(Request $request)
     {
-        $perjanjian_kinerjas = OpdPerjanjianKinerja::whereHas('opd', function ($q) use ($request) {
-            $q->where('nama_opd', $request->nama_opd);
-        })->latest()->first();
+        $id = $request->id;
+        $nama_opd = $request->nama_opd;
+        // $perjanjian_kinerjas = OpdPerjanjianKinerja::whereHas('opd', function ($q) use ($request) {
+        //     $q->where('nama_opd', $request->nama_opd);
+        // })->latest()->first();
+        $perjanjian_kinerja = OpdPerjanjianKinerja::query();
+        if ($id) {
+            $data = $perjanjian_kinerja->where('id', $id)->latest()->first();
+            if ($data == null) {
+                return $this->failedResponse([], 'Oops, Data Tidak Ditemukan');
+            }
+            return $this->successResponse(['perjanjian_kinerja' => new OpdPerjanjianKinerjaResource($data)]);
+        }
+        if ($nama_opd) {
+            $data = $perjanjian_kinerja->whereHas('opd', function ($q) use ($request) {
+                $q->where('nama_opd', $request->nama_opd);
+            })->get();
+        }
         if ($request->nama_opd == null) {
             return $this->failedResponse([], 'Oops, ada yang salah, Pastikan Anda Sudah Mengisi Form Pencarian');
         }
-        if ($perjanjian_kinerjas == null) {
+        if ($data == null) {
             return $this->failedResponse([], 'Oops, ada yang salah, Pastikan Nama Opd Yang Anda Masukkan Benar');
         }
-        return $this->successResponse(['perjanjian_kinerja' => new OpdPerjanjianKinerjaResource($perjanjian_kinerjas)]);
+        return $this->successResponse(['perjanjian_kinerja' => OpdPerjanjianKinerjaResource::collection($data)]);
     }
 }
