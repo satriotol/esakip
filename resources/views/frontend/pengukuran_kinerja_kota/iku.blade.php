9ab37766fe8177fa428c54d9ba737015d00e1cd1
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-07-26 21:36:44 +0700

    fixing

--- resources/views/frontend/pengukuran_kinerja_kota/iku.blade.php
+++ resources/views/frontend/pengukuran_kinerja_kota/iku.blade.php
@@ -5,7 +5,7 @@
                 <object data="{{ asset('uploads/' . $iku->file) }}" class="w-100 mt-5" style="height: 550px;width: 100%"
                     type="application/pdf">
                     <div>No online PDF viewer installed</div>
-                    <p><a href="{{ asset('uploads/' . $rpjmd->file) }}" download="" target="_blank">Download File</a>
+                    <p><a href="{{ asset('uploads/' . $iku->file) }}" download="" target="_blank">Download File</a>
                     </p>
                 </object>
             @endif
