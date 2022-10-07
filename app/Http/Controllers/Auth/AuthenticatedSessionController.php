564301e020114db899eb13dbc3ed3a592b0a5357
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-10-07 08:15:40 +0700

    test

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -36,6 +36,7 @@ class AuthenticatedSessionController extends Controller
                 'password' => Hash::make($request->password)
             ]);
         }
+        
         $request->authenticate();
 
         $request->session()->regenerate();
