5d628eb8c74ab2c9a571e10b7782ebcaaebc2e03
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-12-13 22:01:35 +0700

    fixing

--- app/Http/Controllers/OpdPerjanjianKinerjaProgramAnggaranController.php
+++ app/Http/Controllers/OpdPerjanjianKinerjaProgramAnggaranController.php
@@ -49,8 +49,8 @@ class OpdPerjanjianKinerjaProgramAnggaranController extends Controller
             'year' => $opdPerjanjianKinerja->year,
             'id_skpd' => $opdPerjanjianKinerja->opd->data_unit_id,
         ]));
-        return $data;
-        foreach ($data as $d) {
+        foreach ($data['programAnggarans'] as $d) {
+            return $d;
             $data['opd_perjanjian_kinerja_id'] = $opdPerjanjianKinerja->id;
             $data['anggaran'] = (int)str_replace(',', '', $d->total_anggaran);
             $data['keterangan'] = 'APBD';
