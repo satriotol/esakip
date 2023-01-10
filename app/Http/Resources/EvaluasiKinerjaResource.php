9258a11d04703d0b4c6d26f23226540bca85d14f
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2023-01-10 15:57:12 +0700

    fixing

--- app/Http/Resources/EvaluasiKinerjaResource.php
+++ app/Http/Resources/EvaluasiKinerjaResource.php
@@ -16,11 +16,11 @@ class EvaluasiKinerjaResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'year' => $this->evaluasi_kinerja_year->year,
+            'year' => $this->evaluasi_kinerja_year->year ?? '',
             'value' => $this->value,
-            'category_color' => $this->category_name['color'],
-            'category_name' => $this->category_name['name'],
-            'category_font' => $this->category_name['font_color']
+            'category_color' => $this->category_name['color'] ?? '',
+            'category_name' => $this->category_name['name'] ?? "",
+            'category_font' => $this->category_name['font_color'] ?? '',
         ];
     }
 }
