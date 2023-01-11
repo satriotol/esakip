b5dd335cd7eac9137b0b192dd53021f57371bd86
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2023-01-11 15:11:20 +0700

    FIXING

--- app/Http/Controllers/EvaluasiKinerjaController.php
+++ app/Http/Controllers/EvaluasiKinerjaController.php
@@ -37,7 +37,7 @@ class EvaluasiKinerjaController extends Controller
      */
     public function create($evaluasiKinerjaYear)
     {
-        $opds = Opd::getOpd();
+        $opds = Opd::getOpdExcept();
         return view('evaluasi_kinerja.create', compact('evaluasiKinerjaYear', 'opds'));
     }
 
