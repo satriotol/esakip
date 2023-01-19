1248ac808ceff07371afa511e68a05e2dee6a04c
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2023-01-19 21:19:10 +0700

    fixing

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,7 @@
 @section('content')
     <div class="d-flex justify-content-between align-items-center flex-wrap grid-margin">
         <div>
-            <h4 class="mb-3 mb-md-0">Selamat Datang Mbak Shafa</h4>
+            <h4 class="mb-3 mb-md-0">Selamat Datang Mbak Shafa Pacarnya Mamas</h4>
         </div>
     </div>
 
