eb70ef95e464fba2c2c220c7b69187644e5f13c0
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2023-01-19 21:39:53 +0700

    fixing

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -21,21 +21,20 @@
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="profileDropdown" role="button"
                     data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <img class="wd-30 ht-30 rounded-circle" src="{{ asset('frontend/img/pemkot.png') }}"
-                        alt="profile">
+                    <img class="wd-30 ht-30 rounded-circle" src="{{ asset('frontend/img/pemkot.png') }}" alt="profile">
                 </a>
                 <div class="dropdown-menu p-0" aria-labelledby="profileDropdown">
                     <div class="d-flex flex-column align-items-center border-bottom px-5 py-3">
                         <div class="mb-3">
-                            <img class="wd-80 ht-80 rounded-circle"
-                                src="{{ asset('frontend/img/pemkot.png') }}" alt="">
+                            <img class="wd-80 ht-80 rounded-circle" src="{{ asset('frontend/img/pemkot.png') }}"
+                                alt="">
                         </div>
                         <div class="text-center">
-                            <p class="tx-16 fw-bolder">{{Auth::user()->name}}</p>
-                            <p class="tx-12 text-muted">{{Auth::user()->email}}</p>
+                            <p class="tx-16 fw-bolder">{{ Auth::user()->name }}</p>
+                            <p class="tx-12 text-muted">{{ Auth::user()->email }}</p>
                         </div>
                     </div>
-                    <ul class="list-unstyled p-1">
+                    <ul class="list-unstyled p-1 btn btn-danger w-100">
                         {{-- <li class="dropdown-item py-2">
                             <a href="javascript:;" class="text-body ms-0">
                                 <i class="me-2 icon-md" data-feather="edit"></i>
@@ -50,11 +49,12 @@
                             </a> --}}
                             <form method="POST" action="{{ route('logout') }}">
                                 @csrf
+
                                 <x-responsive-nav-link :href="route('logout')" class="text-decoration-none"
                                     onclick="event.preventDefault();
                                     this.closest('form').submit();"
-                                    style="color: black">
-                                    <i class="me-2 icon-md" data-feather="log-out"></i>
+                                    style="color: white">
+                                    <i class="me-2 icon-md" style="color: white" data-feather="log-out"></i>
                                     <span>Log Out</span>
                                 </x-responsive-nav-link>
                             </form>
