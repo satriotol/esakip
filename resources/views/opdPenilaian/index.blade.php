44b7351d06c480f4c08536d9fb2a4716237cfe4a
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2023-03-21 09:00:39 +0700

    fixing

--- resources/views/opdPenilaian/index.blade.php
+++ resources/views/opdPenilaian/index.blade.php
@@ -29,7 +29,7 @@
                     <div class="text-end mb-2">
                         <a class="btn btn-primary" href="{{ route('opdPenilaian.create') }}">
                             <i data-feather="plus"></i>
-                            Create
+                            Tambah
                         </a>
                     </div>
                     <div class="row">
